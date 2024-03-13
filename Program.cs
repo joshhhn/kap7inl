6906ad485c753f0f516d9406eeed5db86e2625e8
0612jojo <0612jojo@L-5CG2152JXC>
2024-03-13 13:32:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+string input = Console.ReadLine().ToUpper(); // ABCD
+
+
+
+for (int i = 0; i < input.Length; i++)
+{
+    char c = input[i];
+
+    int index = letters.IndexOf(c);
+    Console.Write($"{input[i]}, {index}\n");
+}
